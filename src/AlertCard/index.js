f1b0deca9f0dfcf402e34d45ec15b8f7b1525e41
Kyle Holzinger <kylelholzinger@gmail.com>
2018-05-20 03:21:01 +0200

    [KH] add proptypes to AlertCard

--- src/AlertCard/index.js
+++ src/AlertCard/index.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function AlertCard({ children, className }) {
+function AlertCard({ children, className }) {
   return <div className={`card card--alert ${className}`}>{children}</div>
 }
+
+AlertCard.propTypes = {
+  /** class name to be passed to card */
+  className: PropTypes.string
+}
+
+export default AlertCard
